a2ee62463bd3c34f79404273e3aab1badb825602
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-18 19:33:56 -0700

    add template HTML and JS with divs receiving API request response

--- src/main.js
+++ src/main.js
@@ -3,3 +3,27 @@ import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 
+$(document).ready(() => {
+  $("#weatherLocation").click(() => {
+    const city = $("#location").val();
+    $("#location").val("");
+
+    let request = new XMLHttpRequest();
+    const url = `api.openweathermap.org/data/2.5/weather?q=portland,oregon&appid=a47d1ea44db58ad964bc5b4494638cd3`;
+
+    request.onreadystatechange = () => {
+      if (this.readyState === 4 && this.status === 200) {
+        const response = JSON.parse(this.responseText);
+        getElements(response);
+      }
+    };
+
+    request.open("GET", url, true);
+    request.send();
+
+    function getElements(response) {
+      $('.showHumidity').text(`The humidity in ${city} is ${response.main.humidity}%`);
+      $('.showTemp').text(`The temperature in Kelvins is ${response.main.temp} degrees.`);
+    }
+  });
+});
