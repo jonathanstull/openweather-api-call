4de891f4197127ac858523ee548fa43f042b96ff
Jonathan Stull <jonathan.d.stull@gmail.com>
2021-04-19 22:51:10 -0700

    add weather-service.js separating service logic

--- src/main.js
+++ src/main.js
@@ -2,26 +2,21 @@ import $ from 'jquery';
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
+import WeatherService from './weather-service.js';
+
+function clearFields () {
+  $('#location').val("");
+  $('.showErrors').val("");
+  $('.showHumidity').val("");
+  $('.showTemp').val("");
+}
 
 $(document).ready(() => {
   $("#weatherLocation").click(() => {
-    const city = $("#location").val();
-    $("#location").val("");
+    let city = $("#location").val();
+    clearFields();
 
-    let promise = new Promise(function(resolve, reject) {
-      let request = new XMLHttpRequest();
-      const url = `api.openweathermap.org/data/2.5/weather?q=${city}&appid=${process.env.API_KEY}`;
-      request.onload = function() {
-        if (this.status === 200) {
-          resolve(request.response);
-        } else {
-          reject(request.response);
-        }
-      }
-      request.open("GET", url, true);
-      request.send();
-    });
-
+    let promise = WeatherService.getWeather(city);
     promise.then(function(response) {
       const body = JSON.parse(response);
       $('.showHumidity').text(`The humidity in ${city} is ${body.main.humidity}%`);
@@ -29,8 +24,6 @@ $(document).ready(() => {
       $('.showErrors').text("");
     }, function(error) {
       $('.showErrors').text(`There was an error processing your request: ${error}.`);
-      $('.showHumidity').text("");
-      $('.showTemp').text("");
     });
   });
 });
